5b640505d78e8c258708e8ff5545587212481ab8
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-08 11:10:34 -0700

    yearOfBirth function

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // const myTeam = "Tanner Kyle";
 // console.log(myTeam);
 // console.log("hi");
+
+function yearOfBirth(age =29){
+	return 2017-age;
+}
 function whoAmI(name="Chris", age=29){
-	const yearOfBirth = 2017- age;
+	const yob = yearOfBirth(age);
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-	console.log("I was born in " + yearOfBirth);
+	console.log("I was born in " + yob);
 }
 whoAmI();
 
