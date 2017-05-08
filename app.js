b86fd590db76495f6427cdba254633dfeddbdc0c
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-08 10:57:48 -0700

    added whoAmI function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // const myTeam = "Tanner Kyle";
 // console.log(myTeam);
 // console.log("hi");
-
-console.log("Hi, my name is Chris and I'm 29 years old");
+function whoAmI(){
+	console.log("Hi, my name is Chris and I'm 29 years old");
+}
+whoAmI();
