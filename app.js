9b46c4337d6f96c0da9e86a80448cc6ecad20d3e
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-05-08 11:47:04 -0700

    check if param is same type

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // console.log(myTeam);
 // console.log("hi");
 
-function yearOfBirth(age =29){	
+function yearOfBirth(age){	
 	try{
 		if(age<0){
 			throw new Error("Age can not be negative");
@@ -14,10 +14,21 @@ function yearOfBirth(age =29){
 	return 2017-age;
 
 }
-function whoAmI(name="Chris", age=29){
+function whoAmI(name, age){
+	if(name === undefined || age === undefined){
+		console.error("Arguments not valid");
+	}
+	if(typeof(name) !== String || typeof(age) === NaN){
+		console.error("arguments do not have same type");
+	}
 	const yob = yearOfBirth(age);
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 	console.log("I was born in " + yob);
 }
-whoAmI();
+let a = {
+	age:20,
+	name:"Chris"
+};
+whoAmI("chris",a);
+console.log(typeof(a));
 
